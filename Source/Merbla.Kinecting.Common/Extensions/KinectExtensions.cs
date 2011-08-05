f4db8cd1c3c18fcaa545a1c5c8d07cfbb7f3cd3b
Matthew Erbs <matthew.erbs@merbla.com>
2011-08-05 15:26:15 +1000

    Added intial startup

--- Source/Merbla.Kinecting.Common/Extensions/KinectExtensions.cs
+++ Source/Merbla.Kinecting.Common/Extensions/KinectExtensions.cs
@@ -1,7 +1,37 @@
+using System;
+using System.Windows;
+using Microsoft.Research.Kinect.Nui;
+
 namespace Merbla.Kinecting.Common.Extensions
 {
-    public class KinectExtensions
+    public static class KinectExtensions
     {
-        
+        public static void Initialise(this Runtime nui, Action onSuccess, Action<string> onError)
+        {
+            nui = new Runtime();
+
+            try
+            {
+                nui.Initialize(RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor);
+            }
+            catch (InvalidOperationException)
+            {
+                onError.Invoke("Runtime initialization failed. Please make sure Kinect device is plugged in.");
+                return;
+            }
+            
+            try
+            { 
+                nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+                nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex);
+            }
+            catch (InvalidOperationException)
+            {
+                onError.Invoke("Failed to open stream. Please make sure to specify a supported image type and resolution.");
+                return;
+            }
+
+            onSuccess.Invoke();
+        }
     }
 }
